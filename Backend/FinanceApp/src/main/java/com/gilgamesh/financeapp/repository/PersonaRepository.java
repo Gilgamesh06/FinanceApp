5f8796ade92b2fedbc1d410df2ced578c2ac03b7
Solus <gilgamesh06lg@gmail.com>
2025-10-26 18:16:15 -0500

    Se agrego metodo findByCorreo

--- Backend/FinanceApp/src/main/java/com/gilgamesh/financeapp/repository/PersonaRepository.java
+++ Backend/FinanceApp/src/main/java/com/gilgamesh/financeapp/repository/PersonaRepository.java
@@ -3,8 +3,10 @@ package com.gilgamesh.financeapp.repository;
 import com.gilgamesh.financeapp.model.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona,Long> {
 
+    Optional<Persona> findByCorreo(String correo);
 }
